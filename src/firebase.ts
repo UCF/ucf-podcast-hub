abd5b66f22cea8e8480a2125d07bdaa27e1867eb
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-02-04 13:59:09 -0500

    Removing GA for now

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,11 +12,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
 
 export {
-    analytics,
     db
 }
