79080747bcae458ba056262436a244a875c126d9
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-02-21 11:12:52 -0500

    Split out the functions into separate files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,13 +8,10 @@
  */
 
 import { onCall } from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import { onSchedule } from "firebase-functions/v2/scheduler";
 
-import { initializeApp } from "firebase-admin";
-import { getFirestore } from "firebase-admin/firestore";
-
-import { Parser } from "xml2js";
-import slugify from "slugify";
+import { podcastImport } from "./podcastImport";
+import { addNewShow } from "./addNewShow";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -28,33 +25,9 @@ export const addShow = onCall(async (request) => {
   const url = request.data.url;
   const slug = request.data.slug || null;
 
-  const resp = await fetch(url);
-  if (!resp.ok) logger.error('There was an error retrieving the podcast data.');
-
-  const parser = new Parser();
-  const data = await parser.parseStringPromise(await resp.text());
-
-  const channel = data.rss.channel[0];
-
-  const record = {
-    name: channel.title[0],
-    slug: slug || slugify(channel.title[0], { lower: true, strict: true }),
-    feedUrl: url,
-    description: channel.description[0],
-    owner: channel['podcast:person'][0],
-    image: channel['itunes:image'][0].$.href,
-    episodes: []
-  };
-
-  const app = initializeApp();
-  const db = getFirestore(app);
-
-  const res = await db.collection('shows').doc(record.slug).set(record);
-
-  logger.info(`New show created ${record.name}`, { structuredData: true });
-  return res;
+  await addNewShow(url, slug);
 });
 
-export const episodeImport = onCall(async (request) => {
-
+export const importPodcastEpisodes = onSchedule( "every day 17:00", async (event) => {
+  await podcastImport();
 });
