a76c6988467f960d7805b992d46cdce59f09e9df
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-03-05 09:19:33 -0500

    Removed unused import

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onCall, onRequest } from "firebase-functions/v2/https";
+import { onCall } from "firebase-functions/v2/https";
 // import { onSchedule } from "firebase-functions/v2/scheduler";
 
 import { podcastImport } from "./podcastImport";
